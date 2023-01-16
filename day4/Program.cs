7eb520553933da304dd3c10789e49d0bf7eca081
GreyMister <42612078+GreyMister@users.noreply.github.com>
2023-01-16 08:03:16 +0100

    day4

--- day4/Program.cs
+++ day4/Program.cs
@@ -7,6 +7,7 @@ public class Example
     {
         //variables
         List<string> input_data = new List<string>();      // input data
+        int counter = 0;                                   // found cases (solution)
 
         //getting the input data into a list
         using (var file = new StreamReader("C:\\Users\\Mr. Gutsy\\Desktop\\input4.txt"))
@@ -17,6 +18,64 @@ public class Example
                 input_data.Add(currentline);
             }
         }
+
+        //Getting single values for ranges into two seperate arrays
+        foreach (string s in input_data) {
+            string[] pairs = new string[2];
+            string[] first = new string[2];
+            string[] second = new string[2];
+
+            //Split string into array at ',' (seperating the two ranges)
+            pairs = s.Split(',');
+            
+            //Split both ranges again to get start and end of each range
+            first = pairs[0].Split('-');
+            second = pairs[1].Split('-');
+
+            //Determine larger and smaller range and write each element of each range into corresponding lists
+            List<int> larger = new List<int>();
+            List<int> smaller = new List<int>();
+
+            //second range is larger than first one:
+            if (Int32.Parse(first[1])-Int32.Parse(first[0])+1 < Int32.Parse(second[1])-Int32.Parse(second[0])+1) {
+                for (int i=0; i < Int32.Parse(second[1])-Int32.Parse(second[0])+1;i++) {
+                    larger.Add(Int32.Parse(second[0])+i);
+                }
+                for (int i=0; i < Int32.Parse(first[1])-Int32.Parse(first[0])+1;i++) {
+                    smaller.Add(Int32.Parse(first[0])+i);
+                } 
+            } else {
+            //first range is larger than second one:
+            for (int i=0; i < Int32.Parse(first[1])-Int32.Parse(first[0])+1;i++) {
+                    larger.Add(Int32.Parse(first[0])+i);
+                }
+                for (int i=0; i < Int32.Parse(second[1])-Int32.Parse(second[0])+1;i++) {
+                    smaller.Add(Int32.Parse(second[0])+i);
+                } 
+            }
+
+            //check for each element in shorter range if it is already contained in longer range
+            bool check = false;
+            foreach (int i in smaller) {
+                //if (!larger.Contains(i)) { //Solution for PART 1: Check, if any value in shorter range is not contained in longer range.
+                if (larger.Contains(i)) {    //Solution for PART 2: Check, if any value in shorter range is already in longer range (= overlap).
+                    check = true;
+                    break;
+                }
+            }
+
+            // //PART 1: if "check" == false --> smaller range is completely contained in larger range:
+            // if (check == false) {
+            //     counter +=1;
+            // }
+
+            //PART 2: if "check" == true --> overlap has been found in current line:
+            if (check == true) {
+                counter +=1;
+            }
+
+        }
+        Console.WriteLine(counter);
     }
 }
 
